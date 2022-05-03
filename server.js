c9bab6029f8a2a9dae1822434d44f940a1b6145c
wezerg <vivian.grimaux@hotmail.fr>
2022-05-03 19:08:44 +0200

    Ajout d'une URL images Route pour visiter l'image contenu dans le dossier images

--- server.js
+++ server.js
@@ -8,21 +8,32 @@ const server = http.createServer((req, res) => {
             if (req.method === "GET") {
                 res.writeHead(200, {'content-type':'text/html'});
                 res.write(fs.readFileSync(path.join(__dirname, "public", "pages", "index.html"), {encoding: 'utf-8'}));
+                res.end();
             }
             else{
                 res.writeHead(405, {'content-type':'text/html'});
                 res.write(fs.readFileSync(path.join(__dirname, "public", "pages", "405.html"), {encoding: 'utf-8'}));
+                res.end();
+            }
+        }
+        else if(req.url === "/public/images/image.jpg"){
+            if (req.method === "GET") {
+                res.writeHead(200, {'content-type':'image/jpg'});
+                fs.readFile(path.join(__dirname, "public", "images", "image.jpg"), function(err, data){
+                    res.end(data);
+                });
             }
         }
         else{
             res.writeHead(404, {'content-type':'text/html'});
             res.write(fs.readFileSync(path.join(__dirname, "public", "pages", "404.html"), {encoding: 'utf-8'}));
+            res.end();
         }
     } catch (error) {
         res.writeHead(500, {'content-type':'text/html'});
         res.write(fs.readFileSync(path.join(__dirname, "public", "pages", "500.html"), {encoding: 'utf-8'}));
+        res.end();
     }
-    res.end()
 });
 
 server.listen(3000)
