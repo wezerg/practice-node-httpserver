fb34e95a2c69f855065720fc1deeb142e3c4f4f1
wezerg <vivian.grimaux@hotmail.fr>
2022-05-03 18:13:56 +0200

    Ajout d'un renvoi d'erreur serveur Englobage de toute la logique dans un try catch Renvoi d'une erreur 500 : Erreur serveur

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/") {
-        if (req.method === "GET") {
-            res.writeHead(200, {'content-type':'text/html'});
-            res.write('<h1>Hello World Vivian !</h1>');
+    try {
+        if (req.url === "/") {
+            if (req.method === "GET") {
+                res.writeHead(200, {'content-type':'text/html'});
+                res.write('<h1>Hello World Vivian !</h1>');
+            }
+            else{
+                res.writeHead(405, {'content-type':'text/html'});
+                res.write('<h1>405 : Méthode non authorisée !</h1>');
+            }
         }
         else{
-            res.writeHead(405, {'content-type':'text/html'});
-            res.write('<h1>405 : Méthode non authorisée !</h1>');
+            res.writeHead(404, {'content-type':'text/html'});
+            res.write('<h1>404 : Page introuvable !</h1>');
         }
-    }
-    else{
-        res.writeHead(404, {'content-type':'text/html'});
-        res.write('<h1>404 : Page introuvable !</h1>');
+    } catch (error) {
+        res.writeHead(500, {'content-type':'text/html'});
+        res.write('<h1>500 : Erreur interne au serveur !</h1>');
     }
     res.end()
 });
